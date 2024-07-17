f9f9764595bcd51f21d94177c40dfcf5cb85ccb6
alireza_moghilan <alirezamoghilan0085@gmail.com>
2024-07-17 14:37:03 +0330

    Modification of data receiving codes

--- app/posts/[...id]/page.tsx
+++ app/posts/[...id]/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 // import
-import { client } from "@/component/services/axois";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import { Card, Avatar, Col, Button, Space } from "antd";
+import { Card, Col, Button, Space } from "antd";
 
 // css
 import style from './style.module.scss'
@@ -13,72 +12,67 @@ import LoadingPostId from "@/component/loading/loadingPostId";
 const { Meta } = Card;
 
 export default function PostId({ params }: any) {
+    // interFace
+    interface PostsKey {
+        userId: number,
+        id: number,
+        title: string,
+        body: string
+    }
     // router
     const router = useRouter();
     // state
-    const [posts, setPosts] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [id, setId] = useState(Number(params.id))
+    const [posts, setPosts] = useState<PostsKey>()
+    const [loading, setLoading] = useState<boolean>(false)
+    const [id, setId] = useState<number>(Number(params.id))
+    const [lenOfPosts, setLenOfPosts] = useState<number>(Number(localStorage.getItem('lenOfPosts')))
 
     // get Posts
     const getData = async () => {
-        // Getting data with the axios method
-        // const { data }: any = await client.get('/posts');
-        const data = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: "force-cache" }).then(res => { return res.json() });
-        setPosts(data);
-        setLoading(true)
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`, { cache: "force-cache" });
+        if (response.ok) {
+            const json = await response.json();
+            setPosts(json);
+            setLoading(true);
+        }
     }
 
-    // useEffect => getdata and set postId on the router
+
     useEffect(() => {
-        // set postId on the router
-        router.push(Number(localStorage.getItem('postId') || +1).toString() || '0');
         // Get on page load time
         const fetch = async () => {
             return await getData()
         }
         fetch();
-    }, [])
+    }, []);
 
     // loading ...
     if (!loading) {
-        return LoadingPostId()
+        return LoadingPostId();
     }
-
     return (
         <>
             <h1 className={style.h1Color}>data:{id}</h1>
-            {posts.map((index: any) => {
-                if (id == index.id) {
-                    return (
 
-                        <Col span={12} key={index.id}>
-                            <Card size="small"
-                                title={index.title}
-                                bordered={true}
-                            >
-                                <h3 className="pColor">id Post : {index.id}</h3>
-                                <p>{index.body}</p>
+            <Col span={12} key={posts?.id}>
+                <Card size="small"
+                    title={posts?.title}
+                    bordered={true}
+                >
+                    <h3 className="pColor">id Post : {posts?.id}</h3>
+                    <p>{posts?.body}</p>
 
-                                <Space style={{margin:'16px 8px'}} size={[8, 8]} wrap>
-                                    {/* btn => prive Post */}
-                                    <Button disabled={id<=1?true:false} onClick={() => { return id > 1 ? (setId(id - 1), localStorage.setItem('postId', JSON.stringify(id - 1))) : router.push('1') }}>prive Post</Button>
-                                </Space>
-                                <Space style={{margin:'16px 8px'}} size={[8, 8]} wrap>
-                                    {/* btn => next post */}
-                                    <Button disabled={id>=posts.length?true:false} onClick={() => {
-                                        if (id < posts.length) {
-                                            localStorage.setItem('postId', JSON.stringify(id + 1)); return setId(1 + id);
-                                        }
-                                    }
-                                    }
-                                    >next Post</Button>
-                                </Space>
-                            </Card>
-                        </Col>
-                    )
-                }
-            })}
+                    <Space className={style.spaceBtn} size={[8, 8]} wrap>
+                        {/* btn => prive Post */}
+                        <Button disabled={id <= 1 ? true : false} onClick={() => router.push(Number(--params.id).toString())}>prive Post</Button>
+                    </Space>
+                    <Space className={style.spaceBtn} size={[8, 8]} wrap>
+                        {/* btn => next post */}
+                        <Button disabled={id === lenOfPosts ? true : false} onClick={() => router.push(Number(++params.id).toString())}
+                        >next Post</Button>
+                    </Space>
+                </Card>
+            </Col>
 
         </>
     )
--- component/getPosts.tsx
+++ component/getPosts.tsx
@@ -1,41 +1,56 @@
-import { client } from "@/component/services/axois";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 import { useRouter } from "next/navigation";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import LoadingPosts from "./loading/loadingPosts";
-import { Button, Flex, Card, Col, Space } from 'antd';
+import { Button, Card, Col, Space, Row } from 'antd';
 import { toast } from "react-toastify";
 
 export const GetPostsComponent = () => {
     // router
     const router: AppRouterInstance = useRouter();
     // state
-    const [posts, setPosts] = useState([{}]);
+    const [posts, setPosts] = useState<PostsKey[]>([]);
     const [loading, setLoading] = useState(false);
-    const [viewMore, setViewMore] = useState({ data: false, btn: true });
-    const [viewAll, setViewAll] = useState({ data: false, btn: false });
-    const [viewLess, setViewLess] = useState({ data: false, btn: false });
+
+    const [viewData, setViewData] = useState(
+        {
+            viewMore: { data: false, btn: true, btnText: 'view more' },
+            viewAll: { data: false, btn: false, btnText: 'view all' },
+            viewLess: { data: false, btn: false, btnText: 'view less' },
+            btnTextMain: 'view more'
+        });
+    const [btnClick, setBtnClick] = useState(0);
+
+    // interFace
+    interface PostsKey {
+        id: number,
+        title: string,
+        body: string
+    }
 
     // get posts
     const getData = async () => {
         // Getting data with the axios method
         // const { data }: any = await client.get('/posts');
         try {
-            const data = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: "force-cache" }).then(res => { return res.json() });
-            setPosts(data);
-            setLoading(true)
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts', { cache: "force-cache" })
+                .then(res => {
+                    return res.json();
+                });
+            setPosts(response);
+            setLoading(true);
+            console.log(response.length)
+            localStorage.setItem('lenOfPosts', response.length.toString());
         } catch {
             toast.error(`Sorry, there is a server error.
             Please refresh the page again`);
         }
+
     }
 
     // Received at data load time
     useEffect(() => {
-        const fetch = async () => {
-            return await getData()
-        }
-        fetch()
+        getData();
     }, [])
 
     // loading 
@@ -43,140 +58,95 @@ export const GetPostsComponent = () => {
         return LoadingPosts();
     }
 
-    const viewMoreFun = () => {
-        // show view more
-        setViewMore({ data: true, btn: false });
-
-        // show view all btn
-        setViewAll({ data: false, btn: true });
-        // hide view less
-        setViewLess({ data: false, btn: false });
+    // show data
+    const handlingClicksToViewData = () => {
+        if (btnClick === 0) {
+            setBtnClick(1);
+            setViewData(
+                {
+                    viewMore: { data: true, btn: false, btnText: viewData.viewMore.btnText },
+                    viewAll: { data: false, btn: true, btnText: viewData.viewAll.btnText },
+                    viewLess: { data: false, btn: false, btnText: viewData.viewLess.btnText },
+                    btnTextMain: viewData.viewAll.btnText
+                })
+        } else if (btnClick === 1) {
+            setBtnClick(2);
+            setViewData(
+                {
+                    viewMore: { data: false, btn: false, btnText: viewData.viewMore.btnText },
+                    viewAll: { data: true, btn: false, btnText: viewData.viewAll.btnText },
+                    viewLess: { data: false, btn: true, btnText: viewData.viewLess.btnText },
+                    btnTextMain: viewData.viewLess.btnText
+                })
+        } else if (btnClick === 2) {
+            setBtnClick(0);
+            setViewData(
+                {
+                    viewMore: { data: true, btn: false, btnText: viewData.viewMore.btnText },
+                    viewAll: { data: false, btn: true, btnText: viewData.viewAll.btnText },
+                    viewLess: { data: true, btn: false, btnText: viewData.viewLess.btnText },
+                    btnTextMain: viewData.viewMore.btnText
+                })
+        }
     }
 
-    const viewAllFun = () => {
-        // show view all
-        setViewAll({ data: true, btn: false });
-        // show view less btn
-        setViewLess({ data: false, btn: true });
-        // hide view more
-        setViewMore({ data: false, btn: false });
+    // card antd
+    const card = (id: number, title: string, body: string) => {
+        return (
+            <Col className="h-100" lg={{ span: 8 }} md={{ span: 12 }}>
+                <Card className="h-card" size="small" title={title} bordered={false}>
+                    {body}
+                    <br /><br />
+                    <Space size={[8, 16]} wrap>
+                        <Button className="btn-place" onClick={() => { localStorage.setItem('postId', JSON.stringify(id)); return router.push(`/posts/${id}`) }}>
+                            read more
+                        </Button>
+                    </Space>
+                </Card>
+            </Col>
+        )
     }
 
-    const viewLessFun = () => {
-        // hide view less
-        setViewLess({ data: true, btn: false });
-        // hide view all
-        setViewAll({ data: false, btn: false });
 
-        // hide view more
-        setViewMore({ data: false, btn: true });
-    }
-
+    // 
     const addPosts = (startShowingPosts: number, endShowingPosts: number) => {
-        if (viewMore.data) {
-            return (
-                posts.map((index: any) => (
-                    index.id >= startShowingPosts && index.id <= endShowingPosts &&
-                    <Col span={7} key={index.id}>
-                        <Card size="small" title={index.title} bordered={false}>
-                            {index.body}
-                            <br /><br />
-                            <Space size={[8, 16]} wrap>
-                                <Button onClick={() => { localStorage.setItem('postId', JSON.stringify(index.id)); return router.push(`/posts/${index.id}`) }}>
-                                    read more
-                                </Button>
-                            </Space>
-                        </Card>
-                    </Col>
-                ))
-            )
-        }
-        if (viewAll.data) {
-            return (
-                posts.map((index: any) => (
-                    index.id >= startShowingPosts && index.id <= endShowingPosts &&
-                    <Col span={7} key={index.id}>
-                        <Card size="small" title={index.title} bordered={false}>
-                            {index.body}
-                            <br /><br />
-                            <Space size={[8, 16]} wrap>
-                                <Button onClick={() => { localStorage.setItem('postId', JSON.stringify(index.id)); return router.push(`/posts/${index.id}`) }}>
-                                    read more
-                                </Button>
-                            </Space>
-                        </Card>
-                    </Col>
-                ))
-            )
-        }
-        if (viewLess.data) {
-            return (
-                posts.map((index: any) => (
-                    index.id >= startShowingPosts && index.id <= endShowingPosts &&
-                    <Col span={7} key={index.id}>
-                        <Card size="small" title={index.title} bordered={false}>
-                            {index.body}
-                            <br /><br />
-                            <Space size={[8, 16]} wrap>
-                                <Button onClick={() => { localStorage.setItem('postId', JSON.stringify(index.id)); return router.push(`/posts/${index.id}`) }}>
-                                    read more
-                                </Button>
-                            </Space>
-                        </Card>
-                    </Col>
-                ))
-            )
-        }
+        return (
+            posts.map((items: PostsKey) => (
+                items.id >= startShowingPosts && items.id <= endShowingPosts &&
+                card(items.id, items.title, items.body)
+            ))
+        )
     }
 
     return (
         <>
-            <Flex wrap gap="large" justify={'space-evenly'} align={'start'}>
+            <Row gutter={[24, 24]}>
                 {
                     <>
                         {
-                            !viewLess.data &&
-                            posts.map((index: any) => (
-                                index.id <= 20 &&
-                                <Col span={7} key={index.id}>
-                                    <Card size="small" title={index.title} bordered={false}>
-                                        {index.body}
-                                        <br /><br />
-                                        <Space size={[8, 16]} wrap>
-                                            <Button onClick={() => { localStorage.setItem('postId', JSON.stringify(index.id)); return router.push(`/posts/${index.id}`) }}>
-                                                read more
-                                            </Button>
-                                        </Space>
-                                    </Card>
-                                </Col>
+                            !viewData.viewLess.data &&
+                            posts.map((items: PostsKey) => (
+                                items.id <= 20 &&
+                                card(items.id, items.title, items.body)
                             ))
                         }
                         {
-                            viewMore.data && !viewLess.data &&
+                            viewData.viewMore.data && !viewData.viewLess.data &&
                             addPosts(20, 40)
                         }
                         {
-                            viewAll.data && !viewLess.data &&
+                            viewData.viewAll.data && !viewData.viewLess.data &&
                             addPosts(41, posts.length)
                         }
                         {
-                            viewLess.data &&
+                            viewData.viewLess.data &&
                             addPosts(1, 20)
                         }
                     </>
                 }
-            </Flex>
+            </Row>
             {
-                viewMore.btn &&
-                <Button className="viewMore" style={{ margin: '4rem auto', display: 'flex', padding: '1.5rem 3rem' }} type="primary" onClick={viewMoreFun}>View more</Button>
-            }
-            {
-                viewAll.btn &&
-                <Button className="viewAll" style={{ margin: '4rem auto', display: 'flex', padding: '1.5rem 3rem' }} type="primary" onClick={viewAllFun}>View All</Button>
-            }
-            {
-                viewLess.btn &&
-                <Button className="viewAll" style={{ margin: '4rem auto', display: 'flex', padding: '1.5rem 3rem' }} type="primary" onClick={viewLessFun}>View less</Button>
+                <Button className="viewDataBtn viewMore" type="primary" onClick={handlingClicksToViewData}>{viewData.btnTextMain}</Button>
             }
 
         </>
